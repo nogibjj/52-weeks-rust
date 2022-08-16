5bd99db310a8c940960f515b466a13c524023060
Noah Gift <noah.gift@gmail.com>
2022-08-16 18:20:37 +0000

    adding random choices loop

--- episode4-learn-loops/src/main.rs
+++ episode4-learn-loops/src/main.rs
@@ -1,12 +1,17 @@
-// Looping fun
-// Thank you:  https://rust-lang-nursery.github.io/rust-cookbook/algorithms/randomness.html
-use rand::Rng;
+// Thanks SO:  https://stackoverflow.com/questions/29201370/error-expected-item-found-let
+// This is the crate: https://crates.io/search?q=random_choice
 
-// Broken code fix:  learn to convert to iterator
+extern crate random_choice;
+use self::random_choice::random_choice;
+
 fn main() {
-    //let a = [10, 20, 30, 40, 50];
-    let mut rng = rand::thread_rng();
-    for element in rng {
-        println!("the value is: {element}");
-    }
+let mut samples = vec!["hi", "this", "is", "a", "test!"];
+let weights: Vec<f64> = vec![5.6, 7.8, 9.7, 1.1, 2.0];
+
+let number_choices = 2;
+let choices = random_choice().random_choice_f64(&samples, &weights, number_choices);
+
+for choice in choices {
+    print!("{}, ", choice);
 }
+ }
